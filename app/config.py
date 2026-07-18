1a3d55eabf08108529279d3f8713c79909204b19
anshuman881 <rajputanshuman95@outlook.com>
2026-07-19 02:10:52 +0530

    Fix lambda start up error

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL")
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+DATABASE_URL = os.environ["DATABASE_URL"]
